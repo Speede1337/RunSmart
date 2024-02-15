3df22bc82e8fe43f8e24df5c2944fe7031458e26
Tymur <tborachok@gmail.com>
2024-02-15 20:30:20 +0100

    Add slick and tiny slider

---
+++ src/js/test.js
@@ -0,0 +1,58 @@
+
+// number
+// string - "", '', ``
+// boolean true/false
+// null
+// undefined
+// object 
+// let obj = {
+//   name: 'apple',
+//   color: 'green',
+//   weight: 200,
+// };
+// Symbol
+// let answer = confirm("Are u 18?");
+// console.log(answer);
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2*4 == 8*1) {
+//   console.log("True");
+// } else {
+//   console.log("Error")
+// }
+
+// let answer = confirm("Are u 18?");
+
+// if (answer) {
+//   console.log("Enter");
+// } else {
+//   console.log("Left");
+// }
+
+// const num = 50;
+
+// if(num < 49) {
+//   console.log("Error");
+// } else if (num > 100) {
+//   console.log("a lot");
+// } else {
+//   console.log("true");
+// }
+
+// for(let i = 1; i < 8; i++) {
+//   console.log(i);
+// }
+
+// function logging(a, b) {
+//   console.log(a + b);
+// }
+
+// logging(3 , 5);
+
+// logging(6, 8);
